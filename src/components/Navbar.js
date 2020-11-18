ae86c232d382d17ce2cbe5a8c4956735f28e2f6e
Mandeep <mandeep.99@yahoo.com>
2020-11-18 22:56:15 +0530

    drop down

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,84 +4,109 @@ import { AuthContext } from '../contexts/auth-context';
 import '../jsforproject.js/all';
 
 const HomePage = () => {
-    const { auth } = useContext(AuthContext);
+  const { auth } = useContext(AuthContext);
 
-    return (
-        // <div>
-        <>
-            {/* <div className='navbar-fixed'> */}
-            <nav>
-                <div className='nav-wrapper pcolour'>
-                    <NavLink to='/' className='cleft brand-logo hide-on-med-and-down'>
-                        Logo
+  return (
+    // <div>
+    <>
+      {/* <div className='navbar-fixed'> */}
+      <ul id='dropdown1' className='dropdown-content'>
+        <li>
+          <NavLink to={`/user/${auth.user_id}`} className='stcolour'>
+            My Profile
           </NavLink>
-                    <NavLink to='/' className='brand-logo hide-on-large-only'>
-                        Logo
+        </li>
+        <li>
+          <a href='#!' className='stcolour'>
+            Credit : <span className='ptcolour'>25</span>
+          </a>
+        </li>
+        <li class='divider'></li>
+        <li>
+          <NavLink to='/logout' className='stcolour'>
+            Logout
           </NavLink>
-                    <a href='#!' data-target='mobile-demo' className='sidenav-trigger'>
-                        <i className='material-icons'>menu</i>
-                    </a>
-                    <ul className='right hide-on-med-and-down'>
-                        {auth?.isLoggedIn && (
-                            <>
-                                <li>
-                                    <NavLink to='/appointment'>Appointment</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to='/check'>Monitor Oxygen</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to='/logout'>Logout</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to={`/user/${auth.user_id}`}>My Account</NavLink>
-                                </li>
-                            </>
-                        )}
-                        {!auth?.isLoggedIn && (
-                            <>
-                                <li>
-                                    <NavLink to='/login'>Login</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to='/signup'>Signup</NavLink>
-                                </li>
-                            </>
-                        )}
-                    </ul>
-                </div>
-            </nav>
-            {/* </div> */}
-            <ul className='sidenav' id='mobile-demo'>
-                {auth?.isLoggedIn && (
-                    <>
-                        <li>
-                            <NavLink to='/appointment'>Appointment</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to='/check'>Monitor Oxygen</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to='/logout'>Logout</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={`/user/${auth.user_id}`}>My Account</NavLink>
-                        </li>
-                    </>
-                )}
-                {!auth?.isLoggedIn && (
-                    <>
-                        <li>
-                            <NavLink to='/login'>Login</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to='/signup'>Signup</NavLink>
-                        </li>
-                    </>
-                )}
-            </ul>
-        </>
-    );
+        </li>
+      </ul>
+      <nav>
+        <div className='nav-wrapper pcolour'>
+          <NavLink to='/' className='cleft brand-logo hide-on-med-and-down'>
+            Logo
+          </NavLink>
+          <NavLink to='/' className='brand-logo hide-on-large-only'>
+            Logo
+          </NavLink>
+          <a href='#!' data-target='mobile-demo' className='sidenav-trigger'>
+            <i className='material-icons'>menu</i>
+          </a>
+          <ul className='right hide-on-med-and-down'>
+            {!auth?.isLoggedIn && (
+              <>
+                <li>
+                  <NavLink to='/appointment'>Appointment</NavLink>
+                </li>
+                <li>
+                  <NavLink to='/check'>Monitor Oxygen</NavLink>
+                </li>
+                <li>
+                  <a
+                    className='dropdown-trigger'
+                    href='#!'
+                    data-target='dropdown1'
+                  >
+                    Dropdown
+                    <i className='material-icons right'>arrow_drop_down</i>
+                  </a>
+                </li>
+              </>
+            )}
+            {!auth?.isLoggedIn && (
+              <>
+                <li>
+                  <NavLink to='/login'>Login</NavLink>
+                </li>
+                <li>
+                  <NavLink to='/signup'>Signup</NavLink>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      </nav>
+      {/* </div> */}
+      <ul className='sidenav' id='mobile-demo'>
+        {auth?.isLoggedIn && (
+          <>
+            <li>
+              <NavLink to='/appointment'>Appointment</NavLink>
+            </li>
+            <li>
+              <NavLink to='/check'>Monitor Oxygen</NavLink>
+            </li>
+            <li>
+              <NavLink to='/logout'>Logout</NavLink>
+            </li>
+            <li>
+              <a className='dropdown-trigger' href='#!' data-target='dropdown1'>
+                Dropdown
+                <i className='material-icons right'>arrow_drop_down</i>
+              </a>
+            </li>
+          </>
+        )}
+        {!auth?.isLoggedIn && (
+          <>
+            <li>
+              <NavLink to='/login'>Login</NavLink>
+            </li>
+            <li>
+              <NavLink to='/signup'>Signup</NavLink>
+            </li>
+          </>
+        )}
+      </ul>
+    </>
+  );
 };
 
 export default HomePage;
--- src/jsforproject.js/all.js
+++ src/jsforproject.js/all.js
@@ -1,10 +1,13 @@
 import M from 'materialize-css/dist/js/materialize.min.js';
 document.addEventListener('DOMContentLoaded', function () {
-  console.log("inside");
+  console.log('inside');
   var elems = document.querySelectorAll('.sidenav');
   M.Sidenav.init(elems, {});
 });
 document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.modal');
   M.Modal.init(elems, { dismissible: false });
+});
+document.addEventListener('DOMContentLoaded', function () {
+  M.AutoInit();
 });
