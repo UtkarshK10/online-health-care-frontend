ff709ead6f50cc79dc7125bdacbe367d2c23fb30
Mandeep <mandeep.99@yahoo.com>
2020-11-18 17:41:03 +0530

    drop front

--- src/components/Oxymeter.js
+++ src/components/Oxymeter.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import DropZoneArea from './FileUpload';
+import Spinner from './Spinner';
 
 const Oxymeter = () => {
   const [oxylevel, setlevel] = useState('');
+  const [loading, setloading] = useState(false);
   return (
     <div className='container'>
       <div className='row'>
         <div className='col s12 m12 l12'>
           <h3>Upload the video below</h3>
-          <DropZoneArea />
+          {!loading && <DropZoneArea />};{loading && <Spinner />};
           {oxylevel !== '' && (
             <h4>
               Your blood oxygen level is{' '}
