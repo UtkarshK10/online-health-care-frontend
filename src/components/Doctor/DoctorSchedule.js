b443cdad597bbc72be6cfed4256ed0fcb9bf352e
prakhar <prakhar.kamal2018@vitstudent.ac.in>
2020-12-07 00:23:06 +0530

    url changed

--- src/components/Doctor/DoctorSchedule.js
+++ src/components/Doctor/DoctorSchedule.js
@@ -179,7 +179,7 @@ const DoctorSchedule = () => {
                         disabled={patientDetail.attended}
                         onClick={(e) => {
                           e.preventDefault();
-                          handleClick(patientDetail?.patient_email);
+                          handleClick(patientDetail?.patient_email, patientDetail.patient_record_id);
                         }}
                         className='btn btn-medium pcolour btn-register waves-effect waves-light hover'
                       >
