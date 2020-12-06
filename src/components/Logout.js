fcb2d06f42fd5fe1d1fa6f998e43166051429048
Mandeep <mandeep.99@yahoo.com>
2020-12-06 21:14:06 +0530

    log out error

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -3,19 +3,6 @@ import { useHistory } from 'react-router-dom';
 import { clearLocalStorage } from '../utils/helper';
 import { AuthContext } from '../contexts/auth-context';
 
-<<<<<<< HEAD
-const Logout = () => {
-    const history = useHistory();
-    const { setAuth } = useContext(AuthContext);
-    useEffect(() => {
-        setAuth(null);
-        clearLocalStorage();
-        return () => {
-            history.replace('/login');
-            window.location.reload();
-        };
-    }, [setAuth, history]);
-=======
 const Logout = ({ isDoctor }) => {
   const history = useHistory();
   const { setAuth } = useContext(AuthContext);
@@ -28,9 +15,8 @@ const Logout = ({ isDoctor }) => {
       window.location.reload();
     };
   }, [setAuth, history]);
->>>>>>> 736f57c467171356649034616136b1eee46927cf
 
-    return null;
+  return null;
 };
 
 export default Logout;
